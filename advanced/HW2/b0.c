b2fbe7b36665211ecbc942cb51d9cb1cade1833e
Tony Mantana <butorin.anton@wb.ru>
2025-09-26 23:57:20 +0300

    HW2 added small two tasks

---
+++ advanced/HW2/b0.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+typedef struct list
+{
+    uint64_t address;
+    size_t size;
+    char comment[64];
+    struct list *next;
+} list;
+
+uint64_t findMaxBlock(list *head)
+{
+    uint64_t max_address = 0;
+    size_t max_value = 0;
+    if (head == NULL) {
+        return 0;
+    }
+
+    for (list *block = head; block != NULL; block = block->next)
+    {
+        if (block->size > max_value)
+        {
+            max_value = block->size;
+            max_address = block->address;
+        }
+    }
+
+    return max_address;
+}
+
+size_t totalMemoryUsage(list *head) {
+    size_t sum_size = 0;
+    if (head == NULL) {
+        return 0;
+    }
+
+    for (list *block = head; block != NULL; block = block->next)
+    {
+        sum_size += block->size;
+    }
+
+    return sum_size;
+}
+
+int main(int argc, char *argv[])
+{
+    // Создание узлов (блоков)
+    list item_l = { .address = 140525067852320, .size = 10, .comment = "Block L" };
+    list item_b = { .address = 140525067852350, .size = 30, .comment = "Block B" };
+    list item_a = { .address = 140525067852900, .size = 100, .comment = "Block A" }; // Максимальный размер
+
+    // Соединение узлов в список (L -> B -> A -> NULL)
+    item_l.next = &item_b;
+    item_b.next = &item_a;
+    item_a.next = NULL; // Обязательно завершить список указателем NULL
+
+    // Передаем указатель на начало списка
+    uint64_t result_address = findMaxBlock(&item_l); 
+
+    // Вывод результата: используем PRId64 для корректного вывода uint64_t
+    printf("Result Address: %" PRIu64 "\n", result_address);
+    // Для проверки, какой размер был найден:
+    // printf("Max Size: %zu\n", findMaxBlock(&item_l)); // Для вывода размера нужно изменить функцию или логику
+
+    return 0;
+}
